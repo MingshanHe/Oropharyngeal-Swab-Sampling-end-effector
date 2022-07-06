a994a9b188bea49b7cebb6c7b2e563eb2dcb8262
MingshanHe <hemingshan_1999@163.com>
2022-07-06 16:14:58 +0800

    add install dependency bash file

---
+++ install_dependency.sh
@@ -0,0 +1,8 @@
+ros_distro="melodic"
+sudo apt-get install -y ros-${ros_distro}-industrial-robot-status-interface
+sudo apt-get install -y ros-${ros_distro}-scaled-joint-trajectory-controller
+sudo apt-get install -y ros-${ros_distro}-speed-scaling-interface
+sudo apt-get install -y ros-${ros_distro}-speed-scaling-state-controller
+sudo apt-get install -y ros-${ros_distro}-ur-msgs
+sudo apt-get install -y ros-${ros_distro}-pass-through-controllers
+sudo apt-get install -y ros-${ros_distro}-ur-client-library
